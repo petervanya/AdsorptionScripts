d7defabb3e068ea83be5b00dc1b0e7fdbf4f2c2c
petervanya <peter.vanya@gmail.com>
2015-03-17 12:03:19 +0000

    parse output from Gaussian outfiles

--- parse_output.sh
+++ parse_output.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 # =====
-# Summarising output for each Pt cluster and each spin config
-# spin | converged? | cycles
+# Summarising output of each spin config for a Pt cluster 
+# from Gaussian outfiles
+# Spin | converged? | N. cycles | Conv. error | Runtime
 # 1st cmd ln arg -- cluster, e.g. 12_7
 # =====
 
-dir=../Plain
+dir=~/Adsorption/Plain
 cluster=Pt$1
-cd $cluster
+cd $dir/$cluster
 
 #echo -e "# Spin \t Conv \t Num. cycles \t Err \t Runtime"
 
 for i in {0..10}
 do
-  if [ `tail -n 1 $dir/S_$i/$cluster.out | awk '{print $1}'` == "Normal" ]; then
+  if [ `tail -n 1 S_$i/$cluster.out | awk '{print $1}'` == "Normal" ]; then
     conv="Yes"
   else
     conv="No"
   fi
-  cycles=`cat $dir/S_$i/$cluster.out | grep "cycles$" | awk '{print $5 "\t" $(NF-1)}'`
-  err=`cat $dir/S_$i/$cluster.out | grep "Conv=" | awk '{print substr($(NF-2),6,8)}'`
+  cycles=`cat S_$i/$cluster.out | grep "cycles$" | awk '{print $5 "\t" $(NF-1)}'`
+  err=`cat S_$i/$cluster.out | grep "Conv=" | awk '{print substr($(NF-2),6,8)}'`
   # printing time: $4=days, $6=hours, $8=minutes, $10=seconds
-  runtime=`grep "^ Job cpu" $dir/S_$i/$cluster.out | awk '{print ($4*24*60*60 + $6*60*60 + $8*60 + $10)/60}'`
+  runtime=`cat S_$i/$cluster.out` | grep "^ Job cpu" | awk '{print ($4*24*60*60 + $6*60*60 + $8*60 + $10)/60}'`
 
 	echo -e $i "\t" $conv "\t" $cycles "\t" $err "\t" $runtime
 done  
