a99f37c04e27cc6627415657810f407cb5b30241
petervanya <peter.vanya@gmail.com>
2015-04-13 18:29:52 +0100

    Minor changes

--- cottrell.sh
+++ cottrell.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# USAGE
-# =====
-# -- 1st cmd ln arg: dir name "Water" or "Plain"
-# -- 2nd cmd ln arg: file name without extension
-# =====
+usage(){
+  echo "Usage: $0
+  Script to submit Gaussian adsorption runs to the Cottrell
+
+  Arguments:
+  1st  directory name "Water" or "Plain"
+  2nd  subdirectory name, e.g. Pt9_10_9/Eta_*/S_*
+  3rd  file name without extension"
+  exit 0
+}
+
+if [ $1 == "-h" ]; then
+  usage
+fi
 
 # Generic Cottrell header
 # =======================
@@ -20,8 +29,9 @@ export g09root GAUSS_SCRDIR GAUSS_EXEDIR
 . $g09root/g09/bsd/g09.profile
 # =======================
 
-dir=$1                    # "Water" or "Plain"
-config=$2                 # e.g. Pt9_10_9/Eta_*/S_*
+dir=$1
+config=$2
+file=$3
 
 cd /home/pv278/Platinum/$dir/$config
-/home/Gaussian/g09/g09 < Pt.gjf > Pt.out
+/home/Gaussian/g09/g09 < $file.gjf > $file.out
