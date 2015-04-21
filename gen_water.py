d8c9bb1a31b2b0cb7b6b460807994e93aa49372c
petervanya <peter.vanya@gmail.com>
2015-04-21 13:41:56 +0100

    Aesthetics

--- gen_water.py
+++ gen_water.py
@@ -22,51 +22,23 @@ def printH2O(coordsH2O,filename):
 				s += "%.5f" % coordsH2O[i,j] + "\t"
 			f.write(s+"\n")
 		f.close()
+		print "Water coords printed into file",water.xyz
 
 # ===== parse arguments
-help_text = "Generate xyz water coordinates in yz plane."
-sign_off = "Author: pv278@cam.ac.uk"
-parser = argparse.ArgumentParser(description=help_text,epilog=sign_off)
+help_text="Generate xyz water coordinates in yz plane."
+parser=argparse.ArgumentParser(description=help_text,epilog="Author: pv278@cam.ac.uk")
 
-parser.add_argument(
-				"-pPt",
-				"--posPt",
-				dest="posPt",
-				action="store",
-				type=str,
-				help="Position of O atom in Pt lattice vectors in the form 'a1_a2_a3'",
-				metavar="p"
-)
+parser.add_argument("-pPt","--posPt",dest="posPt",action="store",type=str,
+				            metavar="p",help="Position of O atom in Pt lattice vectors in the form a1_a2_a3")
 
-parser.add_argument(
-				"-p",
-				"--pos",
-				dest="pos",
-				action="store",
-				type=str,
-				help="Shift of O above Pt atom in the form 'x_y_z'",
-				metavar="p"
-)
+parser.add_argument("-p","--pos",dest="pos",action="store",type=str,
+                    metavar="p",help="Shift of O above Pt atom in the form 'x_y_z'")
 
-parser.add_argument(
-				"-phi",
-				dest="angle_phi",
-				action="store",
-				type=float,
-				default=0.0,
-				help="Rotation around z-axis (phi) in degrees (done first)",
-				metavar="t"
-)
+parser.add_argument("-phi",dest="angle_phi",action="store",type=float,default=0.0,
+                    metavar="t",help="Rotation around z-axis (phi) in degrees (done first)")
 
-parser.add_argument(
-				"-theta",
-				dest="angle_theta",
-				action="store",
-				type=float,
-				default=0.0,
-				help="Rotation around y-axis (theta) in degrees",
-				metavar="t"
-)
+parser.add_argument("-theta",dest="angle_theta",action="store",type=float,default=0.0,
+                    metavar="t",help="Rotation around y-axis (theta) in degrees")
 
 arguments = parser.parse_args()
 
@@ -120,9 +92,8 @@ print "Rotated by theta = ",degrees(theta)
 
 coords += np.matlib.repmat(dist,3,1)      # shift all atoms by dist
 
-# ===== print into file
+# ===== print to file
 filename="water.xyz"
 printH2O(coords,filename)
-print "Water coords printed into file water.xyz"
 
 
