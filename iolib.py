c2bd259041991dcb34821282ab9d7d01920c25bf
petervanya <peter.vanya@gmail.com>
2015-05-13 23:33:11 +0100

    Added bandgap functions

--- iolib.py
+++ iolib.py
@@ -37,6 +37,23 @@ def save_table(A, filepath, header=False):
     f.close()
     print "Table saved to",filepath
 
+def print_table(A,header=""):
+    if header:
+        print header
+    M,N = A.shape
+    for i in range(M):
+        line=""
+        for j in range(N):
+            line += str(A[i][j]) + "\t"
+        print line
+
+def get_path(Pt_dir, cluster, spin, eta=0):
+    """get full file path with eta and spin"""
+    if eta != 0:
+        path = Pt_dir + "Water" + "/Pt" + str(cluster) + "/Eta_" + str(eta) + "/S_" + str(spin) + "/Pt.out"
+    else:
+        path = Pt_dir + "Plain" + "/Pt" + str(cluster) + "/S_" + str(spin) + "/Pt.out"
+    return path
 
 def shift(coords,s):
     """shift coordinates by a given vector s"""
@@ -62,5 +79,43 @@ def rotate_phi(coords,phi):
     for i in range(N):
         coords[i,:] = np.dot(Rphi,coords[i,:])
     return coords
+
+
+def get_bandgap(cluster, spin, dir="/home/pv278/Platinum/"):
+    """Extract band gap (highest occ value - lowest virt value)
+       and from the files for a specific spin"""
+    outfile = open(get_path(dir, cluster, spin)).readlines()
+    line = [l for l in outfile if "occ" in l]
+    if line:
+        Eocc = line[-1].split()[-1]
+    else:
+        Eocc = None
+    line = [l for l in outfile if "virt" in l]
+    if line:
+        Evirt = line[0].split()[4]
+    else:
+        Evirt = None
+    
+    if Eocc:
+        return (float(Evirt) - float(Eocc))*27.211
+    else:
+        return
+
+def get_all_bandgaps(cluster, spin_list, dir="/home/pv278/Platinum/"):
+    """Extract band gaps for all spins"""
+    E = []
+    s = []
+    for spin in spin_list:
+        Ebg = get_bandgap(cluster, spin, dir)
+        if Ebg:
+            E.append(Ebg)
+            s.append(spin)
+
+    A = np.vstack((s,E))
+    return A.T
+
+
+
+
 
 
